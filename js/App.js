9aa55e545aa9b277e773ffe57adbc3313e93b59b
Rebecca G. Vereb <Rebecca.Vereb@gmail.com>
2012-05-01 10:26:08 -0600

    removed the picker

--- js/App.js
+++ js/App.js
@@ -10,63 +10,6 @@ Ext.define('BurnChartApp', {
     appName:'Burn Chart',
     cls:'burnchart',
 
-    initComponent: function() {
-        this.callParent(arguments);
-        var piTree = Ext.widget('rallytree', {
-            id: 'rallytree1',
-            width: 400,
-            height: '100%',
-            topLevelModel: 'PortfolioItem',
-            childModelTypeForRecordFn: function(record){
-                if(record.get('Children') && record.get('Children').length > 0){
-                    return 'Portfolio Item';
-                }
-            },
-            parentAttributeForChildRecordFn: function(record){
-                if(record.get('Children') && record.get('Children').length > 0){
-                    return 'Parent';
-                }
-            },
-            listeners: {
-                add: this._onTreeRowAdd,
-                scope: this
-            },
-            topLevelStoreConfig: {
-                listeners: {
-                    beforeload: function(store) {
-                        this.getEl().mask('Loading...');
-                    },
-                    load: function(store, records) {
-                        if (records.length > 0) {
-                            this.add({
-                                id: 'chartCmp',
-                                xtype: 'component',
-                                flex: 1,
-                                html: '<div>Choose a Portfolio Item from the list to see its burn chart.</div>'
-                            })
-                        }
-                        this.getEl().unmask();
-                    },
-                    scope: this
-                }
-            },
-            //override
-            drawEmptyMsg: function(){
-                if (Ext.getCmp('chartCmp')) {
-                    Ext.getCmp('chartCmp').destroy();
-                }
-                this.add({
-                    xtype: 'component',
-                    html: '<p> No Portfolio Items within the currently scoped project(s).</p>'
-                });
-            }
-        });
-        this.add(piTree);
-
-        //add the click handler to tree rows when they are added to the tree
-        //this.down('#rallytree1').on('add', this._onTreeRowAdd, this);
-    },
-
     launch: function () {
         this.startTime = '2012-03-01T00:00:00Z';
         this.chartQuery = {
@@ -80,6 +23,7 @@ Ext.define('BurnChartApp', {
         };
 
         this.chartConfigBuilder = Ext.create('Rally.app.analytics.BurnChartBuilder');
+		this._refreshChart(Rally.environment.getContext().getScope().project.ObjectID, "Test");
 
     },
 
@@ -109,24 +53,7 @@ Ext.define('BurnChartApp', {
         }
     },
 
-    _onTreeRowAdd: function(tree, treeRow) {
-        treeRow.on('afterrender', this._afterTreeRowRendered, this);
-    },
-
-    _afterTreeRowRendered: function(treeRow) {
-        treeRow.getEl().on('click', this._onTreeRowClick, this, {stopEvent: true});
-    },
-
-    _onTreeRowClick: function(event, treeRowTextEl) {
-        var treeItem = Ext.getCmp(Ext.get(treeRowTextEl).findParentNode('.treeItem').id);
-        var treeRowRecord = treeItem.getRecord();
-        var itemId = treeRowRecord.get('ObjectID');
-        var title = treeRowRecord.get('FormattedID') + ' - ' + treeRowRecord.get('Name');
-        this._refreshChart(treeRowRecord, itemId, title);
-    },
-
-    _refreshChart: function(treeRowRecord, itemId, title) {
-        this.selectedRowRecord = treeRowRecord;
+    _refreshChart: function(itemId, title) {
         this.chartQuery.find._ItemHierarchy = itemId;
         this.down('#chartCmp').getEl().mask('Loading...');
         this.chartConfigBuilder.build(this.chartQuery, title, Ext.bind(this._afterChartConfigBuilt, this));
